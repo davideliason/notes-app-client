a8931f6cd7f2b58fda3ec47811d94036a47d8826
David Eliason <davethemaker@gmail.com>
2019-01-30 22:09:40 -0800

    add config to Amplify re: backend resources

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
     s3: {
-      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+      REGION: "us-east-1",
+      BUCKET: "s3-serverless-notes-app"
     },
     apiGateway: {
-      REGION: "YOUR_API_GATEWAY_REGION",
-      URL: "YOUR_API_GATEWAY_URL"
+      REGION: "us-east-1",
+      URL: "https://c058eeluud.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
-      REGION: "YOUR_COGNITO_REGION",
-      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_QReI5yBOf",
+      APP_CLIENT_ID: "1bkd2u5ep9qol3c3csgrjoi1id",
+      IDENTITY_POOL_ID: "us-east-1:aa464bc8-abd7-4c86-b253-88ab4f6a7bef"
     }
   };
